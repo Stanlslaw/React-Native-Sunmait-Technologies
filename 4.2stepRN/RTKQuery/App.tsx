4fa303fcea11834e3352321ed74d6ffae6e37702
Stanislaw Skalkovich <stas.skalkovich@gmail.com>
2024-02-21 17:01:44 +0300

    fix: fix src/Navigation -> src/navigation

--- 4.2stepRN/RTKQuery/App.tsx
+++ 4.2stepRN/RTKQuery/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import {View} from 'react-native';
 import {Provider} from 'react-redux';
 
-import RootNavigation from './src/Navigation/RootNavigation.tsx';
+import RootNavigation from './src/navigation/RootNavigation.tsx';
 import store from './src/redux/store.ts';
 
 function App(): React.JSX.Element {
